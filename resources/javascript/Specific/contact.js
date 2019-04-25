b6c9b0b3bec15525c5254f397e09f52dced5ed1d
boyd brefeld <boydbrefeld@hotmail.com>
2019-04-25 19:39:38 +0200

    modulartest5

--- resources/javascript/Specific/contact.js
+++ resources/javascript/Specific/contact.js
@@ -9,10 +9,12 @@ window.onload = function() {
 const getModules = selectedModules => {
   for (i=0; i<selectedModules.length; i++) {
     const xhr = new XMLHttpRequest();
+    console.log('./resources/html/Modules/'+selectedModules[i]+'.html')
     xhr.open('GET', './resources/html/Modules/'+selectedModules[i]+'.html', true);
     xhr.onreadystatechange= function() {
       if (this.readyState!==4) return;
       if (this.status!==200) return;
+      console.log(xhr.status);
       if (!document.getElementById(selectedModules[i]+"CSS")) {
         let link = document.createElement("link");
         link.id = selectedModules[i]+"CSS";
