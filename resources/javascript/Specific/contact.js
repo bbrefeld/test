4ee0731a41fefa3ade3cee1c5b5d62a2e10abf2b
boyd brefeld <boydbrefeld@hotmail.com>
2019-04-25 20:12:02 +0200

    modulartest7

--- resources/javascript/Specific/contact.js
+++ resources/javascript/Specific/contact.js
@@ -6,28 +6,30 @@ window.onload = function() {
   getModules(selectedModules);
 }
 
-const getModules = selectedModules => {
+const getModules = async selectedModules => {
   for (i=0; i<selectedModules.length; i++) {
     const xhr = new XMLHttpRequest();
     console.log('./resources/html/Modules/'+selectedModules[i]+'.html')
     xhr.open('GET', './resources/html/Modules/'+selectedModules[i]+'.html', true);
-    xhr.onreadystatechange= function() {
+
+    console.log(selectedModules[i]+"CSS");
+    if (!document.getElementById(selectedModules[i]+"CSS")) {
+      let link = document.createElement("link");
+      link.id = selectedModules[i]+"CSS";
+      link.rel = "stylesheet";
+      link.href = "resources/css/Modules/"+selectedModules[i]+".css";
+      console.log(link);
+      document.head.appendChild(link);
+    };
+
+    await xhr.onreadystatechange= function() {
       console.log(xhr.readyState)
       if (xhr.readyState!==4) return;
       console.log(xhr.status);
       if (xhr.status!==200) return;
-      console.log(selectedModules[i]+"CSS");
-      if (!document.getElementById(selectedModules[i]+"CSS")) {
-        let link = document.createElement("link");
-        link.id = selectedModules[i]+"CSS";
-        link.rel = "stylesheet";
-        link.href = "resources/css/Modules/"+selectedModules[i]+".css";
-        console.log(link);
-        document.head.appendChild(link);
-      };
       console.log(xhr.responseText);
       document.getElementById(selectedModules[i]).innerHTML= xhr.responseText;
-    };
+      };
     xhr.send();
   };
 }
