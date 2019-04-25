8120ddbf6dad12d32ec458cb023d0003e34e4cd5
boyd brefeld <boydbrefeld@hotmail.com>
2019-04-25 21:15:05 +0200

    modulartest13

--- resources/javascript/Specific/contact.js
+++ resources/javascript/Specific/contact.js
@@ -1,38 +1,35 @@
 //Modules to load
-const selectedModules = ['header','footer','modal'];
+const modulesToLoad = ['header','footer','modal'];
 
 //Run scripts when page is loaded
 window.onload = function() {
-  getModules(selectedModules);
-}
+  getHtml(modulesToLoad);
+  getCss(modulesToLoad);
+};
 
-const getModules = selectedModules => {
-  for (i=0; i<selectedModules.length) {
+const getHtml = selectedModules => {
+  let i=0;
+  while (i<selectedModules.length) {
     const xhr = new XMLHttpRequest();
-    console.log('./resources/html/Modules/'+selectedModules[i]+'.html')
     xhr.open('GET', './resources/html/Modules/'+selectedModules[i]+'.html', true);
+    xhr.onreadystatechange= function() {
+      if (xhr.readyState!==4) return;
+      if (xhr.status!==200) return;
+      document.getElementById(selectedModules[i]).innerHTML= xhr.responseText;
+      i++;
+      };
+    xhr.send();
+  }
+};
 
-    console.log(selectedModules[i]+"CSS");
+const getCss = selectedModules => {
+  for (i=0; i<selectedModules.length; i++) {
     if (!document.getElementById(selectedModules[i]+"CSS")) {
       let link = document.createElement("link");
       link.id = selectedModules[i]+"CSS";
       link.rel = "stylesheet";
       link.href = "resources/css/Modules/"+selectedModules[i]+".css";
-      console.log(link);
       document.head.appendChild(link);
-    };
-
-    xhr.onreadystatechange= function() {
-      console.log(xhr.readyState)
-      if (xhr.readyState!==4) return;
-      console.log(xhr.status);
-      if (xhr.status!==200) return;
-      console.log(xhr.responseText);
-      console.log(i);
-      console.log(document.getElementById(selectedModules[i]));
-      document.getElementById(selectedModules[i]).innerHTML= xhr.responseText;
-      i++;
-      };
-    xhr.send();
-  };
-}
+    }
+  }
+};
