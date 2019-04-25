f1042726470c5c09c0fd62bff7de39881af064a4
boyd brefeld <boydbrefeld@hotmail.com>
2019-04-25 19:11:41 +0200

    modulartest1

---
+++ resources/javascript/Specific/contact.js
@@ -0,0 +1,27 @@
+//Modules to load
+const selectedModules = [header,footer,modal];
+
+//Run scripts when page is loaded
+window.onload = function() {
+  getModules();
+}
+
+const getModules = selectedModules => {
+  for (i=0; i<selectedModules.length; i++) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', './resources/html/Modules/'+selectedModules[i]+'.html', true);
+    xhr.onreadystatechange= function() {
+      if (this.readyState!==4) return;
+      if (this.status!==200) return;
+      if (!document.getElementById(selectedModules[i]+"CSS")) {
+        let link = document.createElement("link");
+        link.id = selectedModules[i]+"CSS";
+        link.rel = "stylesheet";
+        link.href = "resources/css/Modules/"+selectedModules[i]+".css";
+        document.head.appendChild(link);
+      };
+      document.getElementById(selectedModules[i]).innerHTML= this.responseText;
+    };
+    xhr.send();
+  }
+}
