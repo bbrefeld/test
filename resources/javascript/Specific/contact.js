be43077a4fe39f2afa9e6e2cdb76994c13c501f4
bbrefeld <boydbrefeld@hotmail.com>
2019-04-28 21:55:42 +0200

    changed dynamics javascript

--- resources/javascript/Specific/contact.js
+++ resources/javascript/Specific/contact.js
@@ -6,10 +6,9 @@ window.onload = function(){
 
     var f = (function(){
         var xhr = [], i;
-        for(i = 0; i < modulesToLoad.length; i++){ //for loop
-            (function(i){
+        for(i = 0; i < modulesToLoad.length; i++) {
+            (function(i) {
                 xhr[i] = new XMLHttpRequest();
-                console.log("URLRUNS")
                 url = './resources/html/Modules/'+modulesToLoad[i]+'.html';
                 xhr[i].open("GET", url, true);
                 xhr[i].onreadystatechange = function(){
@@ -26,22 +25,6 @@ window.onload = function(){
 
 };
 
-/*const getHtml = (selectedModules) => {
-  const xhr = [];
-  for (i=0; i<selectedModules.lengh; i++) {
-    (function(i) {
-      xhr[i] = new XMLHttpRequest();
-      xhr[i].open('GET', './resources/html/Modules/'+selectedModules[i]+'.html', true);
-      xhr[i].onreadystatechange= function() {
-        if (xhr[i].readyState === 4 && xhr[i].status === 200) {
-          console.log("test"+i);
-          document.getElementById(selectedModules[i]).innerHTML= this.responseText;
-        };
-      };
-      xhr[i].send();
-    })(i);
-  }
-};*/
 
 const getCss = selectedModules => {
   for (i=0; i<selectedModules.length; i++) {
@@ -54,21 +37,3 @@ const getCss = selectedModules => {
     }
   }
 };
-
-/*const getHtml2 = () => {
-  const f = (function(){
-    const xhr = [], i;
-    for(i = 0; i < 3; i++){ //for loop
-      (function(i){
-        xhr[i] = new XMLHttpRequest();
-        xhr[i].open('GET', './resources/html/Modules/'+selectedModules[i]+'.html', true);
-        xhr[i].onreadystatechange = function(){
-          if (xhr[i].readyState === 4 && xhr[i].status === 200){
-            document.getElementById(selectedModules[i]).innerHTML= this.responseText;
-          }
-        };
-        xhr[i].send();
-      })(i);
-    }
-  })();
-};*/
