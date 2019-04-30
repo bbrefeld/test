92e3b1ce308380ffdd7002bc1c48672718b34762
boyd brefeld <boydbrefeld@hotmail.com>
2019-04-30 14:38:38 +0200

    getHtml inside function test2

--- resources/javascript/Specific/contact.js
+++ resources/javascript/Specific/contact.js
@@ -7,22 +7,24 @@ window.onload = function(){
     getCss(modulesToLoad);
 };
 
-const getHtml = (function(){
-  let xhr = [], i;
-  for(i = 0; i < modulesToLoad.length; i++) {
-    (function(i) {
-      xhr[i] = new XMLHttpRequest();
-      url = './resources/html/Modules/'+modulesToLoad[i]+'.html';
-      xhr[i].open("GET", url, true);
-      xhr[i].onreadystatechange = function(){
-        if (xhr[i].readyState === 4 && xhr[i].status === 200) {
-          document.getElementById(modulesToLoad[i]).innerHTML= this.responseText;
-        }
-      };
-      xhr[i].send();
-    })(i);
-  }
-})();
+const getHtml = () => {
+  (function(){
+    let xhr = [], i;
+    for(i = 0; i < modulesToLoad.length; i++) {
+      (function(i) {
+        xhr[i] = new XMLHttpRequest();
+        url = './resources/html/Modules/'+modulesToLoad[i]+'.html';
+        xhr[i].open("GET", url, true);
+        xhr[i].onreadystatechange = function(){
+          if (xhr[i].readyState === 4 && xhr[i].status === 200) {
+            document.getElementById(modulesToLoad[i]).innerHTML= this.responseText;
+          }
+        };
+        xhr[i].send();
+      })(i);
+    }
+  })();
+};
 
 const getCss = selectedModules => {
   for (i=0; i<selectedModules.length; i++) {
